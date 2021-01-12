87eb073d378ed03cc625d3265d81928a9420d4c9
LGCaparros <lg.caparros@iesdoctorbalmis.com>
2021-01-12 16:54:48 +0100

    Segundo dia de trabajo completado

---
+++ JuegoPeliculas/JuegoPeliculas/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace JuegoPeliculas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Pelicula> listaPeliculas;
+        public MainWindow()
+        {
+            InitializeComponent();
+            listaPeliculas = new ObservableCollection<Pelicula>();
+            listaPeliculasListBox.ItemsSource = listaPeliculas;
+        }
+
+        private void cargarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON file (*.json)|*.json";
+            string json;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using (StreamReader r = new StreamReader(openFileDialog.FileName))
+                {
+                    json = r.ReadToEnd();
+                }
+                listaPeliculas = JsonConvert.DeserializeObject<ObservableCollection<Pelicula>>(json);
+                listaPeliculasListBox.ItemsSource = listaPeliculas;
+            }
+            
+        }
+
+        private void gurdarJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON file (*.json)|*.json";
+
+            string json = JsonConvert.SerializeObject(listaPeliculas);
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, json);
+            }
+            
+        }
+
+        private void listaPeliculasListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            gestionarTabItem.DataContext = listaPeliculasListBox.SelectedItem;
+            eliminarPeliculaJSON.IsEnabled = true;
+        }
+
+        private void añadirPeliculaJSON_Click(object sender, RoutedEventArgs e)
+        {
+            Pelicula peli = new Pelicula("SInSon","Son amarillos ", @"D:\Emu\PPSSPP\memstick\PSP\SAVEDATA\ULES01372DLCTEX\ICON0.PNG", Pelicula.TipoDificultad.Normal, Pelicula.GenerosCine.CienciaFiccion);
+            listaPeliculas.Add(peli);
+        }
+
+        private void eliminarPeliculaJSON_Click(object sender, RoutedEventArgs e)
+        {
+            listaPeliculas.Remove((Pelicula)listaPeliculasListBox.SelectedItem);
+        }
+
+        private void pistaPeliculatextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void examinarImagenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imagenPeliculaTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ JuegoPeliculas/JuegoPeliculas/Pelicula.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoPeliculas
+{
+    class Pelicula : INotifyPropertyChanged
+    {
+		public enum GenerosCine
+		{
+			Comedia,
+			Drama,
+			Accion,
+			Terror,
+			CienciaFiccion
+		}
+
+		public enum TipoDificultad
+		{
+			Facil,
+			Normal,
+			Dificil
+		}
+
+		private string _titulo;
+		public string Titulo
+		{
+			get { return _titulo; }
+			set 
+			{
+				_titulo = value;
+				this.NotifyPropertyChanged("Titulo");
+			}
+		}
+
+		private string _pista;
+		public string Pista
+		{
+			get { return _pista; }
+			set
+			{
+				_pista = value;
+				this.NotifyPropertyChanged("Pista");
+			}
+		}
+
+		private string _imagen;
+
+		public string Imagen
+		{
+			get { return _imagen; }
+			set
+			{
+				_imagen = value;
+				this.NotifyPropertyChanged("Imagen");
+			}
+		}
+
+		private TipoDificultad _dificultad;
+
+		public TipoDificultad Dificultad
+		{
+			get { return _dificultad; }
+			set
+			{
+				_dificultad = value;
+				this.NotifyPropertyChanged("Dificultad");
+			}
+		}
+
+		private GenerosCine _genero;
+
+		public Pelicula()
+		{
+		}
+
+		public Pelicula(string titulo, string pista, string imagen, TipoDificultad dificultad, GenerosCine genero)
+		{
+			Titulo = titulo ?? throw new ArgumentNullException(nameof(titulo));
+			Pista =  pista ?? throw new ArgumentNullException(nameof(pista));
+			Imagen = imagen ?? throw new ArgumentNullException(nameof(imagen));
+			Dificultad = dificultad;
+			Genero = genero;
+		}
+
+		public GenerosCine Genero
+		{
+			get { return _genero; }
+			set
+			{
+				_genero = value;
+				this.NotifyPropertyChanged("Genero");
+			}
+		}
+
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
